8bd5bc61fda478445071ca8f329279793c234f2a
chotipongp <chotipong.p@mail.kmutt.ac.th>
2018-05-16 04:36:01 +0700

    create navbar and searchbox tool + insert multiflix logo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    console.log("This is my initializer")
+
+    this.state = {rows: <p>This is my row</p>}
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <table className="titleBar">
+          <tbody>
+            <tr>
+              <td>
+                <img alt="app icon" width="100" src="multiflix-logo.png"/>
+              </td>
+              <td width="8">
+                <h2>Multiflix</h2>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <input style={{
+          fontSize: 24,
+          display: 'block',
+          width: "99%",
+          paddingTop: 8,
+          paddingBottom: 8,
+          paddingLeft: 16
+        }} placeholder="Enter search term"/>
+
+        {this.state.rows}
+      
       </div>
     );
   }
