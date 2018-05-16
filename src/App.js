65f6f31a487120d1658df42e91eccfb5979a038c
chotipongp <chotipong.p@mail.kmutt.ac.th>
2018-05-17 01:35:16 +0700

    Consuming JSON and Handling Search

--- src/App.js
+++ src/App.js
@@ -7,37 +7,26 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {}
-
-    // const movies = [
-    //   {id:0, poster_src:"https://scontent.fbkk2-2.fna.fbcdn.net/v/t1.0-9/20841916_1535473193176887_2167621013389612117_n.jpg?_nc_cat=0&_nc_eui2=AeGvKu7G_wha5h48uThPFoQR359zFVRZE4jAvjVwBxCVGUf9F0DpgOqNIFm3pSY58NA3CwGfUnJ1Wulx1yEockGfypsRIV3Gej9l12x0j-Y2uA&oh=72f1d99b9f7f939074d1faf727abb1aa&oe=5B903653", title: "Avenger: Infinity War", overview: "dfsdgfdgdfgdfgdfgdfg"}
-    // ]
-
-    // var movieRows = []
-
-    // movies.forEach((movie) => {
-    //   console.log(movie.title)
-    //   const movieRow = <MovieRow movie={movie}/>
-      
-    //   movieRows.push(movieRow)
-    // })
-
-    // this.state = {rows : movieRows}
-
-    this.performSearch()
+    this.performSearch("")
   }
 
-  performSearch() {
+  performSearch(searchTerm) {
     console.log("Perform search using moviedb")
-    const urlString = "https://api.themoviedb.org/3/search/movie?api_key=1b5adf76a72a13bad99b8fc0c68cb085&query="
+    const urlString = "https://api.themoviedb.org/3/search/movie?api_key=1b5adf76a72a13bad99b8fc0c68cb085&query=" + searchTerm
     $.ajax({
       url: urlString,
       success: (searchResults) => {
         console.log("Fetched data successfully")
-        // console.log(searchResults)
         const results = searchResults.results
-        // console.log(result[0])
 
-        results.forEach()
+        var movieRows = []
+
+        results.forEach((movie) => {
+          movie.poster_src = "https://image.tmdb.org/t/p/w185" + movie.poster_path
+          const movieRow = <MovieRow key={movie.id} movie={movie}/>
+          movieRows.push(movieRow)
+        })
+        this.setState({rows: movieRows})
       },
       error: (xhr, status, err) => {
         console.error("Failed to fetch data")
@@ -45,18 +34,24 @@ class App extends Component {
     })
   }
 
+  searchChangeHandler(event) {
+    console.log(event.target.value)
+    const boundObject = this
+    const searchTerm = event.target.value
+    boundObject.performSearch(searchTerm)
+  }
+
   render() {
     return (
       <div>
+        
         <table className="titleBar">
           <tbody>
             <tr>
               <td>
-                <img alt="app icon" width="100" src="multiflix-logo.png"/>
+                <img alt="app icon" width="50%" src="multiflix-logo.png"/>
               </td>
-              <td width="8">
-                <h2>Multiflix</h2>
-              </td>
+              <td width="8"/>
             </tr>
           </tbody>
         </table>
@@ -68,13 +63,14 @@ class App extends Component {
           paddingTop: 8,
           paddingBottom: 8,
           paddingLeft: 16
-        }} placeholder="Enter search term"/>
+        }} onChange={this.searchChangeHandler.bind(this)} placeholder="Enter search term"/>
 
         {this.state.rows}
-      
+
       </div>
     );
   }
 }
+
 
 export default App;
